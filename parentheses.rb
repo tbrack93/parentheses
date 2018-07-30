64ea6c3463b69e009b5d6032ed1a3630f33e974c
Tim Brackfield <Cloud_193@hotmail.com>
2018-07-30 12:30:47 +0100

    added additional comment

--- parentheses.rb
+++ parentheses.rb
@@ -2,6 +2,7 @@ def valid_parentheses(string)
   characters = string.split("")
   # initial basic balance check
   return false if characters.count("(") != characters.count(")")
+  #full check
   count = 0
   characters.each do |character|
     count += 1 if character == "("
